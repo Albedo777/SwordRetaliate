122c29125f0def2dbad54be1874ec1076e0fd7ab
jialonwang <jialonwang@tencent.com>
2022-09-15 17:44:57 +0800

    新增AICharacter

---
+++ Source/SwordRetaliate/Public/SwordRetaliateAICharacter.h
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+//#include "PaperFlipbook.h"
+#include "SwordRetaliateAICharacter.generated.h"
+
+/**
+ * 
+ */
+
+
+UCLASS()
+class SWORDRETALIATE_API ASwordRetaliateAICharacter : public APaperCharacter
+{
+	GENERATED_BODY()
+
+	///** Side view camera */
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	//	class UCameraComponent* SideViewCameraComponent;
+
+	///** Camera boom positioning the camera beside the character */
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	//	class USpringArmComponent* CameraBoom;
+
+
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+	//	class UPaperFlipbook* FastRunAnimation;
+
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+	//	class UPaperFlipbook* SlowRunAnimation;
+
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+	//	class UPaperFlipbook* JumpAnimation;
+
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+	//	class UPaperFlipbook* HitAnimation;
+
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+	//	class UPaperFlipbook* VertigoAnimation;
+
+
+protected:
+//
+	virtual void Tick(float DeltaSeconds) override;
+//
+	void UpdateAnimation();
+//
+	void MoveRight(float Value);
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+		class UPaperFlipbook* RunAnimation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Animations)
+		class UPaperFlipbook* SkillAnimation;
+//
+//public: 
+//
+	ASwordRetaliateAICharacter();
+
+	///** Returns SideViewCameraComponent subobject **/
+	//FORCEINLINE class UCameraComponent* GetSideViewCameraComponent() const { return SideViewCameraComponent; }
+	///** Returns CameraBoom subobject **/
+	//FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	
+};
