f519ba0e4cceb0ce9873dccca240fd47f8e85c4f
ryangzli <ryangzli@tencent.com>
2022-09-20 17:11:12 +0800

    UI+树叶特效+火球碰撞

---
+++ Source/SwordRetaliate/SwordRetaliateUtils.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "SwordRetaliateCharacter.h"
+#include "SwordRetaliateUtils.generated.h"
+
+UCLASS()
+class SWORDRETALIATE_API USwordRetaliateUtils : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	static ASwordRetaliateCharacter* GetSwordRetaliateCharacter(const UObject* WorldContextObject);
+};
---
+++ Source/SwordRetaliate/UI/UIController.cpp
@@ -0,0 +1,46 @@
+﻿#include "UIController.h"
+
+#include "Components/Button.h"
+#include "SwordRetaliate/SwordRetaliateUtils.h"
+#include "SwordRetaliate/SkillSystem/SkillComponent.h"
+
+void UUIController::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	FirstSkillButton->OnClicked.AddUniqueDynamic(this, &UUIController::CastFirstSkill);
+	SecondSkillButton->OnClicked.AddUniqueDynamic(this, &UUIController::CastSecondSkill);
+	JumpButton->OnClicked.AddUniqueDynamic(this, &UUIController::OnClickJump);
+}
+
+void UUIController::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+}
+
+void UUIController::NativeDestruct()
+{
+	FirstSkillButton->OnClicked.RemoveAll(this);
+	SecondSkillButton->OnClicked.RemoveAll(this);
+	JumpButton->OnClicked.RemoveAll(this);
+	
+	Super::NativeDestruct();
+}
+
+void UUIController::CastFirstSkill()
+{
+	const ASwordRetaliateCharacter* LocalPawn = USwordRetaliateUtils::GetSwordRetaliateCharacter(this);
+	LocalPawn->SkillComponent->CastSkill(FirstSkillId);
+}
+
+void UUIController::CastSecondSkill()
+{
+	const ASwordRetaliateCharacter* LocalPawn = USwordRetaliateUtils::GetSwordRetaliateCharacter(this);
+	LocalPawn->SkillComponent->CastSkill(SecondSkillId);
+}
+
+void UUIController::OnClickJump()
+{
+	ASwordRetaliateCharacter* LocalPawn = USwordRetaliateUtils::GetSwordRetaliateCharacter(this);
+	LocalPawn->Jump();
+}
